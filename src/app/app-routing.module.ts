cf9ae93e3340613258df0eed605d99893afdb8da
Dipti Choudhari <dipti.choudhari25@gmail.com>
2023-06-11 19:48:55 +0530

    first project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RestaurentDashComponent } from './restaurent-dash/restaurent-dash.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'login',pathMatch: 'full' //login
+  },
+  {
+   path: 'login', component: LoginComponent  // login
+  },
+ {
+   path: 'signup', component: SignupComponent //signup
+ }, 
+ {
+   path:'restaurent' , component: RestaurentDashComponent //restarent-dash
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
